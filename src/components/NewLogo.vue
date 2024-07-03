c278e96c6e8d9f8ced5dadbabb2742f138593dd7
Dfcurie <102874523+DFCURIE@users.noreply.github.com>
2024-07-03 20:33:11 +0700

    update login page

--- src/components/NewLogo.vue
+++ src/components/NewLogo.vue
@@ -1,25 +1,25 @@
 <template>
-    <img :src="logoSrc" :height="height" alt="New Logo" />
-  </template>
-  
-  <script lang="ts" setup>
-  import { defineProps } from 'vue'
-  
-  const props = defineProps<{
-    height?: number
-  }>()
-  
-  // Import the logo image using the ES6 import statement
-  import logoImage from '@/assets/new-logo.png'
-  
-  const logoSrc = logoImage
-  </script>
-  
-  <style scoped>
-  img {
-    display: block;
-    max-width: 100%;
-    height: auto;
-  }
-  </style>
-  
+  <img :src="logoSrc" :height="height" :width="width" alt="New Logo" />
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+  height?: number
+  width?: number
+}>()
+
+// Import the logo image using the ES6 import statement
+import logoImage from '@/assets/new-logo.png'
+
+const logoSrc = logoImage
+</script>
+
+<style scoped>
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+</style>
