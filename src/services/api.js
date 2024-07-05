dfeaf50ffe79b4ffec74038948c098c26dcf0e19
Dfcurie <102874523+DFCURIE@users.noreply.github.com>
2024-07-05 15:48:22 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -21,8 +21,9 @@ api.interceptors.request.use(config => {
 
 export const login = async (email, password) => {
   try {
+    console.log('Sending login request:', { email, password })
     const response = await api.post('/login', { email, password });
-    localStorage.setItem('token', response.data.data.token); // Save the token
+    console.log('Login response:', response)
     return response.data;
   } catch (error) {
     console.error('Error during login:', error);
@@ -37,59 +38,6 @@ export const logout = async (token) => {
     return response.data;
   } catch (error) {
     console.error('Error during logout:', error);
-    throw error;
-  }
-};
-
-
-
-// User management
-export const getUsers = async (params) => {
-  try {
-    const response = await api.get('/user', { params });
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching users:', error);
-    throw error;
-  }
-};
-
-export const getUser = async (id) => {
-  try {
-    const response = await api.get(`/user/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching user:', error);
-    throw error;
-  }
-};
-
-export const addUser = async (userData) => {
-  try {
-    const response = await api.post('/user', userData);
-    return response.data;
-  } catch (error) {
-    console.error('Error creating user:', error);
-    throw error;
-  }
-};
-
-export const updateUser = async (id, userData) => {
-  try {
-    const response = await api.put(`/user/${id}`, userData);
-    return response.data;
-  } catch (error) {
-    console.error('Error updating user:', error);
-    throw error;
-  }
-};
-
-export const removeUser = async (id) => {
-  try {
-    const response = await api.delete(`/user/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error deleting user:', error);
     throw error;
   }
 };
